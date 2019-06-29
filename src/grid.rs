0045e7044265032c3cd4172bacafb4919a29b6e1
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-06-29 20:52:39 +0200

    Implement utilities for directions

--- src/grid.rs
+++ src/grid.rs
@@ -2,41 +2,53 @@ use std::ops::{Index, IndexMut};
 
 use nalgebra as na;
 
-pub type Pos3 = na::Vector3<usize>;
+pub type Vec3 = na::Vector3<isize>;
 
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct Grid3<T> {
-    size: Pos3,
+    size: Vec3,
     data: Vec<T>,
 }
 
 impl<T: Default + Copy> Grid3<T> {
-    pub fn new(size: Pos3) -> Grid3<T> {
+    pub fn new(size: Vec3) -> Grid3<T> {
+        let n = (size.x * size.y * size.z) as usize;
         Grid3 {
             size,
-            data: vec![Default::default(); size.x * size.y * size.z],
+            data: vec![Default::default(); n],
         }
     }
 }
 
 impl<T> Grid3<T> {
-    pub fn node_index(&self, p: Pos3) -> usize {
-        p.x
-        + p.y * self.size.x
-        + p.z * self.size.x * self.size.y
+    pub fn node_index(&self, p: Vec3) -> usize {
+        debug_assert!(self.is_valid_pos(p));
+
+        let index =
+            p.x +
+            p.y * self.size.x +
+            p.z * self.size.x * self.size.y;
+        
+        index as usize
+    }
+
+    pub fn is_valid_pos(&self, p: Vec3) -> bool {
+        p.x >= 0 && p.x < self.size.x &&
+        p.y >= 0 && p.y < self.size.y &&
+        p.z >= 0 && p.z < self.size.z
     }
 }
 
-impl<T> Index<Pos3> for Grid3<T> {
+impl<T> Index<Vec3> for Grid3<T> {
     type Output = T;
 
-    fn index<'a>(&'a self, p: Pos3) -> &'a T {
+    fn index<'a>(&'a self, p: Vec3) -> &'a T {
         &self.data[self.node_index(p)]
     }
 }
 
-impl<T> IndexMut<Pos3> for Grid3<T> {
-    fn index_mut<'a>(&'a mut self, p: Pos3) -> &'a mut T {
+impl<T> IndexMut<Vec3> for Grid3<T> {
+    fn index_mut<'a>(&'a mut self, p: Vec3) -> &'a mut T {
         let index = self.node_index(p);
         &mut self.data[index]
     }
--- src/machine.rs
+++ src/machine.rs
@@ -1,10 +1,20 @@
-use crate::grid::{Pos3, Grid3};
+use crate::grid::{Vec3, Grid3};
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
-enum Axis {
+enum Axis3 {
     X,
     Y,
     Z,
+}
+
+impl Axis3 {
+    pub fn to_vector(&self) -> Vec3 {
+        match self {
+            X => Vec3::new(1, 0, 0),
+            Y => Vec3::new(0, 1, 0),
+            Z => Vec3::new(0, 0, 1),
+        }
+    }
 }
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
@@ -13,14 +23,32 @@ enum Sign {
     Neg,
 }
 
+impl Sign {
+    pub fn to_number(&self) -> isize {
+        match self {
+            Pos => 1,
+            Neg => -1,
+        }
+    }
+}
+
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
-struct Dir(Axis, Sign);
+struct Dir3(Axis3, Sign);
+
+impl Dir3 {
+    pub fn to_vector(&self, p: Vec3) -> Vec3 {
+        self.0.to_vector() * self.1.to_number()
+    }
+}
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
 enum Block {
-    Pipe(Dir),
+    Pipe {
+        from: Dir3,
+        to: Dir3,
+    },
     Solid,
-    Switch(Dir),
+    Switch(Dir3),
 }
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
@@ -28,7 +56,7 @@ struct BlockId(usize);
 
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
 struct Blip {
-    pos: Pos3,
+    pos: Vec3,
 }
 
 #[derive(PartialEq, Eq, Clone, Debug)]
