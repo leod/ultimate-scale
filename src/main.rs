629b285965fadb556c5cfcfcbd55fbb814ab92f8
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-07-19 02:32:05 +0200

    Some improvements to lighting

--- src/main.rs
+++ src/main.rs
@@ -108,7 +108,7 @@ fn main() {
         if let Some(deferred_shading) = &mut deferred_shading {
             render_lists.lights.push(render::Light {
                 position: na::Point3::new(15.0, 15.0, 15.0),
-                attenuation: na::Vector3::new(0.8, 0.125, 0.03),
+                attenuation: na::Vector3::new(1.0, 0.01, 0.001),
                 color: na::Vector3::new(1.0, 1.0, 1.0),
                 radius: 30.0,
             });
