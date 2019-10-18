8ce76dc29fca053e0589dcd0dbca9a9cffbf0c4a
leod <subtle.frustration@proton.me>
2019-10-18 23:30:56 +0200

    Reenable deferred with shadow mapping

--- src/config.rs
+++ src/config.rs
@@ -27,7 +27,7 @@ pub struct RenderConfig {
 impl Default for RenderConfig {
     fn default() -> RenderConfig {
         RenderConfig {
-            shadow_mapping: None, //Some(Default::default()),
+            shadow_mapping: Some(Default::default()),
             deferred_shading: Some(Default::default()),
         }
     }
