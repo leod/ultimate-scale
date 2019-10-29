b13bb6a38393e3607ee222866d45bc6e79401c20
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-10-29 21:21:16 +0100

    Turn Edit::SetBlock primitive into SetBlocks

--- src/edit/mod.rs
+++ src/edit/mod.rs
@@ -1,7 +1,7 @@
 pub mod config;
 pub mod editor;
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::machine::grid;
 use crate::machine::{Machine, PlacedBlock};
@@ -10,20 +10,30 @@ pub use config::Config;
 pub use editor::Editor;
 
 pub enum Edit {
-    SetBlock(grid::Point3, Option<PlacedBlock>),
+    SetBlocks(HashMap<grid::Point3, Option<PlacedBlock>>),
 }
 
 impl Edit {
     pub fn run(&self, machine: &mut Machine) -> Edit {
         match self {
-            Edit::SetBlock(p, block) => {
-                let previous_block = machine
-                    .get_block_at_pos(p)
-                    .map(|(_index, block)| block)
-                    .cloned();
-                machine.set_block_at_pos(p, block.clone());
+            Edit::SetBlocks(blocks) => {
+                let previous_blocks = blocks
+                    .keys()
+                    .map(|p| {
+                        (
+                            *p,
+                            machine
+                                .get_block_at_pos(p)
+                                .map(|(_index, block)| block.clone()),
+                        )
+                    })
+                    .collect();
 
-                Edit::SetBlock(*p, previous_block)
+                for (p, block) in blocks.iter() {
+                    machine.set_block_at_pos(p, block.clone());
+                }
+
+                Edit::SetBlocks(previous_blocks)
             }
         }
     }
