75c4735d8a196965d2042d027df5b6ab9d011c05
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-10-29 23:58:01 +0100

    Start changing editor to place multiple blocks via Piece

--- src/edit/mod.rs
+++ src/edit/mod.rs
@@ -9,7 +9,69 @@ use crate::machine::{Machine, PlacedBlock};
 pub use config::Config;
 pub use editor::Editor;
 
-pub struct Piece(HashMap<grid::Point3, Option<PlacedBlock>>);
+/// A piece of a machine that can be kept around as edit actions, or in the
+/// clipboard and stuff like that.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Piece {
+    pub rotation_xy: usize,
+    pub blocks: HashMap<grid::Point3, PlacedBlock>,
+}
+
+impl Piece {
+    pub fn new_origin_block(block: PlacedBlock) -> Self {
+        Self::new_rotated_origin_block(0, block)
+    }
+
+    pub fn new_rotated_origin_block(rotation_xy: usize, block: PlacedBlock) -> Self {
+        Self {
+            rotation_xy: rotation_xy,
+            blocks: maplit::hashmap! {
+                grid::Point3::origin() => block,
+            },
+        }
+    }
+
+    pub fn rotate_cw(&mut self) {
+        self.rotation_xy += 1;
+        if self.rotation_xy == 4 {
+            self.rotation_xy = 0;
+        }
+    }
+
+    pub fn rotate_ccw(&mut self) {
+        if self.rotation_xy == 0 {
+            self.rotation_xy = 3;
+        } else {
+            self.rotation_xy -= 1;
+        }
+    }
+
+    pub fn place_edit(&self, offset: &grid::Vector3) -> Edit {
+        let set_blocks = self
+            .iter_blocks(offset)
+            .map(|(pos, block)| (pos, Some(block)))
+            .collect();
+
+        Edit::SetBlocks(set_blocks)
+    }
+
+    pub fn iter_blocks(
+        &self,
+        offset: &grid::Vector3,
+    ) -> impl Iterator<Item = (grid::Point3, PlacedBlock)> + '_ {
+        let offset = *offset;
+        // TODO: Rotate
+        self.blocks.iter().map(move |(pos, block)| {
+            (
+                pos + offset,
+                PlacedBlock {
+                    rotation_xy: block.rotation_xy + self.rotation_xy,
+                    ..block.clone()
+                },
+            )
+        })
+    }
+}
 
 #[derive(Debug, Clone)]
 pub enum Edit {
@@ -53,5 +115,5 @@ impl Edit {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Mode {
     Select(HashSet<grid::Point3>),
-    PlaceBlock(PlacedBlock),
+    PlacePiece(Piece),
 }
