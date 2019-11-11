0be424177a50b3a30e58c1b6f6a183772019d9e7
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-11-11 18:57:08 +0100

    Implement sending level input into the machine

--- src/machine/level.rs
+++ src/machine/level.rs
@@ -1,8 +1,11 @@
+use std::collections::VecDeque;
+
+use rand::Rng;
 use serde::{Deserialize, Serialize};
 
 use crate::machine::{grid, BlipKind};
 
-#[derive(PartialEq, Eq, Copy, Clone, Debug, Serialize, Deserialize)]
+#[derive(PartialEq, Eq, Clone, Debug, Serialize, Deserialize)]
 pub struct Level {
     pub size: grid::Vector3,
     pub spec: Spec,
@@ -10,14 +13,34 @@ pub struct Level {
 
 impl Level {}
 
-pub struct InputOutput {
-    pub input: Vec<Vec<BlipKind>>,
-    pub output: Vec<Vec<BlipKind>>,
+#[derive(PartialEq, Eq, Copy, Clone, Debug, Serialize, Deserialize)]
+pub enum Input {
+    Blip(BlipKind),
 }
 
-#[derive(PartialEq, Eq, Copy, Clone, Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone)]
+pub struct InputsOutputs {
+    pub inputs: VecDeque<Vec<Option<Input>>>,
+    pub outputs: VecDeque<Vec<BlipKind>>,
+}
+
+impl InputsOutputs {
+    pub fn pop_input(&mut self) -> Option<Vec<Option<Input>>> {
+        self.inputs.pop_front()
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Debug, Serialize, Deserialize)]
 pub enum Spec {
     Id { dim: usize },
+}
+
+pub fn gen_random_blip_kind<R: Rng + ?Sized>(rng: &mut R) -> BlipKind {
+    if rng.gen() {
+        BlipKind::A
+    } else {
+        BlipKind::B
+    }
 }
 
 impl Spec {
@@ -33,12 +56,27 @@ impl Spec {
         }
     }
 
-    pub fn generate_input_output(&self) -> InputOutput {
+    pub fn generate_inputs_outputs<R: Rng + ?Sized>(&self, rng: &mut R) -> InputsOutputs {
         match self {
-            Spec::Id { dim } => InputOutput {
-                input: Vec::new(),
-                output: Vec::new(),
-            },
+            Spec::Id { dim } => {
+                let len: usize = rng.gen_range(5, 20);
+
+                let outputs: VecDeque<Vec<_>> = (0..len)
+                    .map(|_| (0..*dim).map(|_| gen_random_blip_kind(rng)).collect())
+                    .collect();
+
+                let inputs = outputs
+                    .iter()
+                    .map(|outputs| {
+                        outputs
+                            .iter()
+                            .map(|kind| Some(Input::Blip(*kind)))
+                            .collect()
+                    })
+                    .collect();
+
+                InputsOutputs { inputs, outputs }
+            }
         }
     }
 }
