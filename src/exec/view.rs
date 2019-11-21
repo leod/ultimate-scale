345f9fc2917edd121473f93edb81375738a3f85e
leod <subtle.frustration@proton.me>
2019-11-21 18:48:38 +0100

    Tune deferred lighting constants a bit

--- src/exec/view.rs
+++ src/exec/view.rs
@@ -329,9 +329,9 @@ impl ExecView {
 
             out.lights.push(render::pipeline::Light {
                 position: pos,
-                attenuation: na::Vector3::new(0.0, 0.0, 100.0),
-                color: na::Vector3::new(0.2, 10.0, 0.5),
-                radius: 10.0,
+                attenuation: na::Vector3::new(1.0, 0.0, 2.0),
+                color: render::machine::blip_color(blip.kind), //na::Vector3::new(0.2, 10.0, 0.5),
+                radius: 2.0,
             });
         }
     }
