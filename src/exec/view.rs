44d8781fd06f1913158bf49e00d850b242ce448b
Leonard Dahlmann <leo.dahlmann@gmail.com>
2019-11-24 14:06:34 +0100

    Have wind sources be light sources when active?

--- src/exec/view.rs
+++ src/exec/view.rs
@@ -334,7 +334,7 @@ impl ExecView {
 
             out.lights.push(render::pipeline::Light {
                 position: pos,
-                attenuation: na::Vector3::new(1.0, 0.0, 2.0),
+                attenuation: na::Vector3::new(1.0, 0.0, 6.0),
                 color: 20.0 * render::machine::blip_color(blip.kind),
                 is_main: false,
             });
